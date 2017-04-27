509fad843642b8566d7b006e774faaea438ef4d1
Jonathan Rossi <jonathan.m.rossi@gmail.com>
2017-04-27 14:39:57 -0700

    Add scroll to top button for docs

--- js/customscripts.js
+++ js/customscripts.js
@@ -53,3 +53,20 @@ $(function() {
         }
     });
 });
+
+
+/** Scroll to Top Functionality **/
+$(document).ready(function() {
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('#scroll-to-top').fadeIn();
+        } else {
+            $('#scroll-to-top').fadeOut();
+        }
+    });
+
+    $('#scroll-to-top').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 400);
+        return false;
+    });
+});
