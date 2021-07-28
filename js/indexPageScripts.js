0cf6d7c91510c513bfa54d202c2f74515f969ef4
ssama-vmware <ssama@vmware.com>
2021-07-28 10:16:31 -0500

    Add get started page that spans whole screen Brand new index page

---
+++ js/indexPageScripts.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    if (window.location.pathname.includes("index")) {
+        document.querySelector("#tg-sb-content").classList.add("index-page-content")
+    }
+});
